b2d4fa089ce20a6a0baf1f0bdb70d5e4004f32a3
Dunstan Becht <dunstan@becht.network>
2022-02-22 18:35:22 +0100

    update examples

--- tests/generate_input.py
+++ tests/generate_input.py
@@ -12,22 +12,26 @@ rdd = RDD, {'d': 5e13*1e-18}
 
 data.export(
     sets.Sample(10, 'square', 3200, *rdd, S=0),
+    a3=5,
     pbc=1,
     expdir='input',
 )
 
 data.export(
     sets.Distribution('square', 3200, *rdd, S=0, c='PBC1'),
+    a3=5,
     expdir='input',
 )
 
 data.export(
     sets.Distribution('circle', 1600, *rdd, S=0, t='edge'),
+    a3=5,
     expdir='input',
 )
 
 data.export(
     sets.Distribution('circle', 1600, *rdd, S=0, c='ISD'),
+    a3=5,
     expdir='input',
 )
 
